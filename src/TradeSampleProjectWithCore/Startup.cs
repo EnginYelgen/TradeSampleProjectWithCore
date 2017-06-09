c90cba975d114127e90923e18d5fdf2ae685dc2b
engin.yelgen <engin.yelgen@PC132>
2017-06-09 09:29:43 +0300

    model and configuration

--- src/TradeSampleProjectWithCore/Startup.cs
+++ src/TradeSampleProjectWithCore/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 using TradeSampleProjectWithCore.Models;
 
 
@@ -19,27 +20,38 @@ namespace TradeSampleProjectWithCore
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //// Add framework services.
-            //services.AddDbContext<TradeSampleContext>(options =>
-            //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            // Add framework services.
+            services.AddDbContext<TradeSampleContext>(options =>
+                options.UseSqlServer(Program.Configuration.GetConnectionString("UniteksConnection")));
 
-            //services.AddMvc();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, TradeSampleContext context)
         {
-            loggerFactory.AddConsole();
+            //loggerFactory.AddConsole();
+            loggerFactory.AddConsole(Program.Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DbInitialize.Initialize(context);
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
